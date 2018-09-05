3ed908f6072048fa054269f74eee16c23a31d437
Alex Cruikshank <alex@carbonfive.com>
2018-09-04 21:39:04 -0400

    add some instruments

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <WS2812B.h>
 
 #define NUM_LEDS 50
+#define NUM_STRIPS 50
 #define EN1 PB9
 #define EN2 PB4
 #define EN3 PA2
@@ -23,7 +24,7 @@
 #define BIKE_LOW 1700
 #define BIKE_HIGH 2200
 #define CYMBAL PB10
-#define CYMBAL_TIME 500
+// #define CYMBAL_TIME 500
 #define NOODLE PA1
 
 // states
@@ -50,22 +51,23 @@
 #define DRUM_SLOPE 18.0
 #define DRUM_ACC .0015
 
+#define CYMBOL_ATTACK 200
+#define CYMBOL_DECAY 2000
+#define CYMBOL_FREQ .0002
+#define R_PHASE -.012
+#define G_PHASE -.028
+#define B_PHASE -.025
 #define PUMP PA0
 
 
 WS2812B strip = WS2812B(NUM_LEDS);
 
+float distances[NUM_LEDS*NUM_STRIPS];
+
 long lastDrumHit[] = {0,0,0,0,0};
+long lastCymbolHit = 0;
 int32_t drumColor[] = {0xfe00000, 0xf9fe000, 0x00fe030, 0x0003fe0, 0x9d00fe0};
-
 int count = 0;
-int enc_a = LOW;
-int enc_b = LOW;
-bool bike_on = false;
-uint64 lastCymbolHit = 0;
-bool cymbalOn = false;
-bool noodleOn = false;
-
 int state = INIT;
 
 // commands
@@ -91,6 +93,14 @@ void setup() {
   digitalWrite(EN1, LOW);
   strip.begin();
   strip.show();
+
+  for (int i=0; i< NUM_STRIPS; i++) {
+    for (int j=0; j< NUM_LEDS; j++) {
+      float dx = 3*(NUM_STRIPS*.5 - i);
+      float dy = (NUM_LEDS*.5 - j);
+      distances[i*NUM_LEDS+j] = sqrt(dx*dx + dy*dy);
+    }
+  }
 
   Serial.begin(9600);
 }
@@ -175,6 +185,14 @@ void readData() {
   // noodleOn = noodle > 1000;
 // }
 
+long attackDecay(long time, long attack, long decay) {
+  if (time < attack)
+    return map(time, 0, attack, 0, 1000);
+  if (time < attack+decay)
+    return map(time-attack, 0, decay, 1000, 0.0);
+  return 0;
+}
+
 void showColor(int stripEnable, uint8 r, uint8 g, uint8 b, int column) {
   // long ctime = min(CYMBAL_TIME, millis() - lastCymbolHit);
   // long cbrightness = map(ctime*ctime, 0, CYMBAL_TIME*CYMBAL_TIME, 255, 0);
@@ -182,18 +200,41 @@ void showColor(int stripEnable, uint8 r, uint8 g, uint8 b, int column) {
   int drum = (int) (column / 10);
   float drumTime = lastDrumHit[drum] - millis();
   float drumDrop = DRUM_ACC * drumTime * drumTime;
+  long cymbolTime = millis()-lastCymbolHit;
+  int env = attackDecay(cymbolTime, CYMBOL_ATTACK, CYMBOL_DECAY);
+  float cfreq = CYMBOL_FREQ * attackDecay(cymbolTime, 0, 2*CYMBOL_ATTACK+CYMBOL_DECAY) / 1000.0;
 
   digitalWrite(stripEnable, HIGH);
   for(uint16_t i=0; i<strip.numPixels(); i++)
   {
-    int pedalVal = 1000 - abs(40*((2*i+count)%50) - 1000);
-    int32_t color = colorMap(pedalVal, r, g, b);
+    // int pedalVal = 1000 - abs(40*((2*i+count)%50) - 1000);
+    // int32_t color = colorMap(pedalVal, r, g, b);
+    uint32_t color = (i+count) % 20 < 5 ? 0x2828282 : 0x000000;
 
     if (drumDrop > 50 - i) {
       int drumVal = constrain(DRUM_OFFSET - DRUM_SLOPE*(drumDrop - 50 + i), 0, 1000);
       color = mixColor(drumVal, color, drumColor[drum]);
     }
-    color = mixColor(constrain(millis()-lastCymbolHit,0,1000), 0xffffff, color);
+
+    // cymbol / stompbox
+    // Atack decay curve base on hit time
+    // 3 concentric sin waves, r, g and b
+    // Same freq, phase is a function of time * c, where c varies slightly for each
+    // for each wave, create a high colors from existing color, c: c|0xFF0000, c|0x00FF00, c|0x0000FF
+    // also create low colors: c&0x00FFFF, c&0xFF00FF, c&0xFFFF00
+    // if val=sin(freq*dist + cPhase*time) > 0, mixColor(val, c, highColor) otherwise mixColor(-val, c, lowColor)
+    // combine colors something like this:
+    if (env > 0) {
+      // this is not actually mixing to zero
+      float dist = distances[column*NUM_LEDS + i];
+      int rval = (int) (env*sin((cfreq*dist + R_PHASE)*cymbolTime));
+      int gval = (int) (env*sin((cfreq*dist + G_PHASE)*cymbolTime));
+      int bval = (int) (env*sin((cfreq*dist + B_PHASE)*cymbolTime));
+      uint32_t r = rval > 0 ? asRed(map(rval,0,1000,fromRed(color), 0xFF)) : asRed(map(-rval,0,1000,fromRed(color), 0x00));
+      uint32_t g = gval > 0 ? asGreen(map(gval,0,1000,fromGreen(color), 0xFF)) : asGreen(map(-gval,0,1000,fromGreen(color), 0x00));
+      uint32_t b = bval > 0 ? asBlue(map(bval,0,1000,fromBlue(color), 0xFF)) : asBlue(map(-bval,0,1000,fromBlue(color), 0x00));
+      color = r | g | b;
+    }
 
     strip.setPixelColor(i, color);
     // int32_t mixedColor = strip.Color(
@@ -232,14 +273,14 @@ void runInteractive() {
   // showColor(EN7,  0, 60, 60, 6+offset);
   // showColor(EN8, 60,  0, 60, 7+offset);
 
-  showColor(EN1, 80, 80, 80, 0+offset);
-  showColor(EN2, 80, 80, 80, 1+offset);
-  showColor(EN4, 80, 80, 80, 2+offset);
-  showColor(EN3, 80, 80, 80, 3+offset);
-  showColor(EN5, 80, 80, 80, 4+offset);
-  showColor(EN6, 80, 80, 80, 5+offset);
-  showColor(EN7, 80, 80, 80, 6+offset);
-  showColor(EN8, 80, 80, 80, 7+offset);
+  showColor(EN1, 40, 40, 40, 0+offset);
+  showColor(EN2, 40, 40, 40, 1+offset);
+  showColor(EN4, 40, 40, 40, 2+offset);
+  showColor(EN3, 40, 40, 40, 3+offset);
+  showColor(EN5, 40, 40, 40, 4+offset);
+  showColor(EN6, 40, 40, 40, 5+offset);
+  showColor(EN7, 40, 40, 40, 6+offset);
+  showColor(EN8, 40, 40, 40, 7+offset);
 }
 
 
